b6d37d54169324e859954365828dad5e8917b6a1
Alexey Shaytan <alex@intbio.org>
2020-01-22 01:26:29 +0300

    Update

--- pytexshade/test_shade.py
+++ pytexshade/test_shade.py
@@ -62,3 +62,81 @@ def test_5z3l():
 )
 		size=os.path.getsize('test_results/5z3l_%s.png'%s)
 		assert size>10000, "output png filesize too small, looks that nothing was produced"
+
+
+def test_logo():
+	#Prepare a Multiple Sequence Alignment in biopython
+human_h2a_z_core=Seq('SRSQRAGLQFPVGRIHRHLKSRTTSHGRVGATAAVYSAAILEYLTAEVLELAGNASKDLKVKRITPRHLQLAIRGDEELDSLI-KATIAGGGVIPHIHKSLIG')
+xenopus_h2a_core=Seq('TRSSRAGLQFPVGRVHRLLRKGNYAE-RVGAGAPVYLAAVLEYLTAEILELAGNAARDNKKTRIIPRHLQLAVRNDEELNKLLGRVTIAQGGVLPNIQSVLLP')
+msa=MultipleSeqAlignment([SeqRecord(xenopus_h2a_core,id='H2A',name='H2A'),SeqRecord(human_h2a_z_core,id='H2AZ',name='H2AZ')])
+#We need to describe feautures
+#See also TexShade docs http://mirrors.mi.ras.ru/CTAN/macros/latex/contrib/texshade/texshade.pdf
+features=[
+    {'style':'fill:$\\uparrow$', #Styles - all TexShade feature types 'helix','loop','-->','---','<--',',-,' AND also "frameblock", "shaderegion" or "shadeblock", see TexShade docs for more info\
+     #'position':'top',# Position of feature annotation also 'bottom','ttop','bbottom', etc. if no - automatic'\
+     #'seqref':1, #number of sequence for selections - default consensus\
+     'sel':[5,10], # selection with 0-based numbering, both start and end are included!\
+     #'color':'Red',# color for for frame block\
+     #'thickness':1.5 #-1.5pt also for frameblock\
+     #'rescol':'Black',  #this is residue color for shaderegion style, see TexShade docs \shaderegion{ seqref  }{ selection }{ res.col. }{ shad.col. }\
+     #'shadcol':'Green',#this is shding color for shaderegion style, see TexShade docs \shaderegion{ seqref  }{ selection }{ res.col. }{ shad.col. }\
+     #funcgroup example fg="\\funcgroup{xxx}{CT}{White}{Green}{upper}{up} \\funcgroup{xxx}{GA}{White}{Blue}{upper}{up}"\
+     'text':'test'},
+     {'style':'helix', \
+     'sel':[25,30], # selection with 0-based numbering, both start and end are included!\
+     'text':'$\\alpha 1$'},
+     {'style':'-->', \
+     'sel':[35,40], # selection with 0-based numbering, both start and end are included!\
+     'text':'$\\beta 2$'},
+]
+#Shade
+	shade_aln2png(msa,\
+ 	filename='test_results/logo.png',\
+ 	 debug=False,\
+     shading_modes=['similar'],# list of shading modes according to TexShade, supported are "similar", "hydropathy_functional", "chemical_functional", "structure_functional", "charge_functional", "diverse"\
+     legend=False,features=features,title='',\
+     logo=True, #SeqLogo \
+     hideseqs=False,#activate \hidseqs command \ 
+     splitN=20, #alignment will be split into splitN batches\
+     setends=[], # a section of alignment will be displayed between setends[0] and setends[1]\
+     ruler=False, # Add a ruler\
+     show_seq_names=True,# Show sequence names\
+     funcgroups=None, # Hilight functional groups, Tex code should be inserted example funcgroups="\\funcgroup{xxx}{CT}{White}{Green}{upper}{up} \\funcgroup{yyy}{GA}{White}{Blue}{upper}{up}" \
+     show_seq_length=True #Show sequence length \
+    )
+
+	size=os.path.getsize('test_results/logo.png'%s)
+	assert size>10000, "output png filesize too small, looks that nothing was produced"
+
+
+def test_long_msa():
+	from Bio import AlignIO
+	from urllib.request import urlopen 
+	import tempfile
+	temp = tempfile.NamedTemporaryFile()
+	pfam_path="https://pfam.xfam.org/family/PF00125/alignment/seed"
+	data = urlopen(pfam_path).read()
+	# print(data)
+	with open(temp.name,'wb') as f:
+		f.write(data)
+	alignment = AlignIO.read(temp.name, "stockholm")
+
+#Shade
+	shade_aln2png(alignment,\
+	filename='test_results/large.png',\
+     shading_modes=['similar'],# list of shading modes according to TexShade, supported are "similar", "hydropathy_functional", "chemical_functional", "structure_functional", "charge_functional", "diverse"\
+     legend=False,features=[],title='',\
+     logo=True, #SeqLogo \
+     hideseqs=False,#activate \hidseqs command \ 
+     splitN=20, #alignment will be split into splitN batches\
+     setends=[], # a section of alignment will be displayed between setends[0] and setends[1]\
+     ruler=False, # Add a ruler\
+     show_seq_names=True,# Show sequence names\
+     funcgroups=None, # Hilight functional groups, Tex code should be inserted example funcgroups="\\funcgroup{xxx}{CT}{White}{Green}{upper}{up} \\funcgroup{yyy}{GA}{White}{Blue}{upper}{up}" \
+     show_seq_length=True #Show sequence length \
+    )
+
+	size=os.path.getsize('test_results/large.png'%s)
+	assert size>10000, "output png filesize too small, looks that nothing was produced"
+
+
